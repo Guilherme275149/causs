f69177d7868a62b24d827c72bad826056a331760
guilherme <78052265+Guilherme275149@users.noreply.github.com>
2021-02-28 21:59:31 -0300

    Delete creator.js

--- plugins/creator.js
+++
@@ -1,9 +0,0 @@
-let handler = function (m) {
-  this.sendContact(m.chat, '5511934488829', 'Nurutomo', m)
-}
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
-
-handler.command = /^(owner|creator)$/i
-
-module.exports = handler
